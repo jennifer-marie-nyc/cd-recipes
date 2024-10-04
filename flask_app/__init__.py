94fc29c96d6188b8bfe5f2eb0235df9a73088516
Jennifer Mitchell <144286546+jennifer-marie-nyc@users.noreply.github.com>
2024-10-04 19:13:06 -0400

    update __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -5,8 +5,6 @@ from flask_bcrypt import Bcrypt
 from os import environ
 
 app = Flask(__name__)
-# secret_key_urlsafe = secrets.token_urlsafe(16)
-# app.secret_key = secret_key_urlsafe
 app.secret_key = environ.get('SECRET_KEY_URLSAFE')
 
 bcrypt = Bcrypt(app)
